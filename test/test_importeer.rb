7f4ea1a9911fcdb300e450571082ea5613fda4c5
rolf <rolf@l-plan.nl>
2015-07-08 14:09:06 +0200

    added tests

--- test/test_importeer.rb
+++ test/test_importeer.rb
@@ -4,17 +4,38 @@ class TestImporteer < Minitest::Test
 
 
 
-  	describe "test that it has a version number" do
 
-	    it "test the obvious" do
-	     refute_nil ::ImporteerPlan::VERSION
+
+  	describe "Importeer" do	
+  		let(:importeer) { ImporteerPlan::Importeer.new('testje.xls') }
+
+	    it "test that importeer exists" do
+	     refute_nil importeer
 	    end
+
+	    it "test name of the file" do
+	     importeer.name.must_equal 'testje.xls'
+	    end
+
+	    it "test size of batch" do
+	     importeer.size_batch.must_equal 1000
+	    end
+
+	    it "test size of batch when declared" do
+	     ImporteerPlan::Importeer.new('testje.xls', 123).size_batch.must_equal 123
+	    end
+
+	    it "test the first line of the file" do
+	     Spreadsheet.open(importeer.path).worksheet(0).row(0).to_a.must_equal ['A', 'B', 'C', 'D', 'E', 'F']
+	 	end
 
 
 
 
 	end
 
+
  
 
 end
+
